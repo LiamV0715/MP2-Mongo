1256cf368e298b434b73e65ee3875cb702f79699
Liam Vermette <Liam.vermette@gmail.com>
2024-07-10 20:04:52 -0400

    fixed pet model to fix comments

--- models/pets.js
+++ models/pets.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 const petSchema = new mongoose.Schema({
   name: { type: String, required: true },
   pic: { type: String, default: 'https://images.unsplash.com/photo-1560553174-28b8e2f73bc1?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-  weight: { type: Number, default: 'mystery weight!' },
+  weight: { type: Schema.Types.Mixed , default: 'mystery weight!', required: false },
   age: { type: String, default: 'Age not found' },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
